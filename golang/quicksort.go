63b07414c309d0c8089e53396c469cdf2d74ff1e
Andreas Briese <ab@bri-c.de>
2016-11-26 20:07:55 +0100

    runtime, int32, handcrafted loop optimization
    Original is considerably slower than C-version compiled with gcc -O3 ( see below )
    
    Including runtime to restrict resource use is for comparability.
    Change from int to int32 for better comparability AND speed optimization.  
    After handcrafting the for-loops in partition() the go binary outperforms the -O3 C executable by about a third.     
    
    My timings are:
    original C-file compiled without compiler optimization
    $ gcc -o qsC c/quicksort.c; ./gsC
    0: 16807, 1: 282475249, 2: 1622650073
    took 28812.257812
    0: 43, 1: 50, 2: 87
    
    original C file compiled with O3 compiler optimization
    $ gcc -O3 -o qsC_O3 c/quicksort.c; ./qsC_O3 
    0: 16807, 1: 282475249, 2: 1622650073
    took 9886.303711
    0: 43, 1: 50, 2: 87
    
    gcc info: 
    Configured with: --prefix=/Applications/Xcode.app/Contents/Developer/usr --with-gxx-include-dir=/usr/include/c++/4.2.1
    Apple LLVM version 5.0 (clang-500.2.79) (based on LLVM 3.3svn)
    Target: x86_64-apple-darwin12.6.0
    Thread model: posix
    
    Go quicksorter
    original
    $ go build -o ./qsGO golang/quicksort1.go; ./qsGO
    a1[0]: 5577006791947779410, a1[1]: 8674665223082153551, a1[2]: 6129484611666145821
    n of 100000000 took 10.920328081s to run.
    a1[0]: 5577006791947779410, a1[1]: 8674665223082153551, a1[2]: 6129484611666145821
    
    optimized
    $ go build -o ./qsGOO golang/quicksort.go; ./qsGOO
    a1[0]: 134020434, a1[1]: 1597969999, a1[2]: 1721070109
    n of 100000000 took 6.050837056 to run.
    a1[0]: 134020434, a1[1]: 1597969999, a1[2]: 1721070109

--- golang/quicksort.go
+++ golang/quicksort.go
@@ -3,29 +3,163 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"runtime"
 	"time"
 )
 
-func swap(x *int, y *int) {
+func swap(x *int32, y *int32) {
 	x, y = y, x
 }
 
-func partition(array []int, p uint, q uint, pivotLocation uint) uint {
+func partition(array []int32, p uint, q uint, pivotLocation uint) uint {
 
 	pivot := array[pivotLocation]
 	swap(&array[pivotLocation], &array[q])
-	i := p
-	for j := p; j < q; j++ {
+	i, j := p, p
+	d := (q - p)
+	if d&15 == 0 { // %16
+		for j = p; j < q; j += 16 {
+			if array[j] <= pivot {
+				swap(&array[i], &array[j])
+				i++
+			}
+			if array[j+1] <= pivot {
+				swap(&array[i], &array[j+1])
+				i++
+			}
+			if array[j+2] <= pivot {
+				swap(&array[i], &array[j+2])
+				i++
+			}
+			if array[j+3] <= pivot {
+				swap(&array[i], &array[j+3])
+				i++
+			}
+			if array[j+4] <= pivot {
+				swap(&array[i], &array[j+4])
+				i++
+			}
+			if array[j+5] <= pivot {
+				swap(&array[i], &array[j+1])
+				i++
+			}
+			if array[j+6] <= pivot {
+				swap(&array[i], &array[j+2])
+				i++
+			}
+			if array[j+7] <= pivot {
+				swap(&array[i], &array[j+3])
+				i++
+			}
+			if array[j+8] <= pivot {
+				swap(&array[i], &array[j])
+				i++
+			}
+			if array[j+9] <= pivot {
+				swap(&array[i], &array[j+1])
+				i++
+			}
+			if array[j+10] <= pivot {
+				swap(&array[i], &array[j+2])
+				i++
+			}
+			if array[j+11] <= pivot {
+				swap(&array[i], &array[j+3])
+				i++
+			}
+			if array[j+12] <= pivot {
+				swap(&array[i], &array[j+4])
+				i++
+			}
+			if array[j+13] <= pivot {
+				swap(&array[i], &array[j+1])
+				i++
+			}
+			if array[j+14] <= pivot {
+				swap(&array[i], &array[j+2])
+				i++
+			}
+			if array[j+15] <= pivot {
+				swap(&array[i], &array[j+3])
+				i++
+			}
+		}
+		swap(&array[q], &array[i])
+		return i
+	}
+	if d&7 == 0 { // %8
+		for j = p; j < q; j += 8 {
+			if array[j] <= pivot {
+				swap(&array[i], &array[j])
+				i++
+			}
+			if array[j+1] <= pivot {
+				swap(&array[i], &array[j+1])
+				i++
+			}
+			if array[j+2] <= pivot {
+				swap(&array[i], &array[j+2])
+				i++
+			}
+			if array[j+3] <= pivot {
+				swap(&array[i], &array[j+3])
+				i++
+			}
+			if array[j+4] <= pivot {
+				swap(&array[i], &array[j+4])
+				i++
+			}
+			if array[j+5] <= pivot {
+				swap(&array[i], &array[j+1])
+				i++
+			}
+			if array[j+6] <= pivot {
+				swap(&array[i], &array[j+2])
+				i++
+			}
+			if array[j+7] <= pivot {
+				swap(&array[i], &array[j+3])
+				i++
+			}
+		}
+		swap(&array[q], &array[i])
+		return i
+	}
+	if d&3 == 0 { // %4
+		for j = p; j < q; j += 4 {
+			if array[j] <= pivot {
+				swap(&array[i], &array[j])
+				i++
+			}
+			if array[j+1] <= pivot {
+				swap(&array[i], &array[j+1])
+				i++
+			}
+			if array[j+2] <= pivot {
+				swap(&array[i], &array[j+2])
+				i++
+			}
+			if array[j+3] <= pivot {
+				swap(&array[i], &array[j+3])
+				i++
+			}
+		}
+		swap(&array[q], &array[i])
+		return i
+	}
+	
+	for j = p; j < q; j += 4 {
 		if array[j] <= pivot {
 			swap(&array[i], &array[j])
 			i++
 		}
 	}
+
 	swap(&array[q], &array[i])
 	return i
 }
 
-func quicksort(array []int, start uint, end uint) {
+func quicksort(array []int32, start uint, end uint) {
 	if start < end {
 		pivot := (end + start) / 2
 		r := partition(array, start, end, pivot)
@@ -37,10 +171,14 @@ func quicksort(array []int, start uint, end uint) {
 }
 
 func main() {
-	a1 := make([]int, 100000000)
+
+	runtime.GOMAXPROCS(2)
+	runtime.LockOSThread()
+
+	a1 := make([]int32, 100000000)
 
 	for i := 0; i < 100000000; i++ {
-		a1[i] = rand.Int()
+		a1[i] = int32(rand.Int())
 	}
 
 	fmt.Printf("a1[0]: %d, a1[1]: %d, a1[2]: %d\n", a1[0], a1[1], a1[2])
@@ -48,7 +186,8 @@ func main() {
 	// now run the test
 	t0 := time.Now()
 	quicksort(a1, 0, 100000000-1)
-	t1 := time.Now()
-	fmt.Printf("n of %v took %v to run.\n", len(a1), t1.Sub(t0))
+	t1 := time.Since(t0)
+	fmt.Printf("n of %v took %v to run.\n", len(a1), t1.Seconds())
 	fmt.Printf("a1[0]: %d, a1[1]: %d, a1[2]: %d\n", a1[0], a1[1], a1[2])
+	
 }
